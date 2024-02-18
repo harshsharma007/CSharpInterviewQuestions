7d0564dadf7978a51445810d161f8772d5f4074c
Harsh Sharma <harshsharmahs@gmail.com>
2024-02-18 21:37:48 +0530

    Question 01

---
+++ Question-01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PassByValueVsReference
+{
+    public class A
+    {
+        public int value;
+
+        public A()
+        {
+            value = 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Increment(int b)
+        {
+            b++;
+        }
+
+        static void Increment(A a)
+        {
+            a.value++;
+        }
+
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Increment(a);
+            int b = a.value;
+            Increment(b);
+
+            Console.WriteLine(a.value);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
